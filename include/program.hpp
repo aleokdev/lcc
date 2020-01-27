3e855339d354902b5597bb1ea2858d5a68abd994
alexdevteam <alexpro820@gmail.com>
2020-01-27 23:29:01 +0100

    Some useless files

--- include/program.hpp
+++ include/program.hpp
@@ -1,5 +1,15 @@
+#ifndef LCC_PROGRAM_HPP__
+#define LCC_PROGRAM_HPP__
 
+#include <filesystem>
 
+namespace lcc {
 class LCProgram {
+public:
+    LCProgram(std::filesystem::path& path);
 
+private:
 };
+} // namespace lcc
+
+#endif
