4fb3dbe0c4a3a7f4853c42d7c604fd2173aceff8
alexdevteam <alexinfdev@gmail.com>
2020-02-15 15:25:43 +0100

    Add multistack class

---
+++ include/lcc/multi_stack.hpp
@@ -0,0 +1,30 @@
+#ifndef LCC_MULTI_STACK_HPP
+#define LCC_MULTI_STACK_HPP
+
+#include <stack>
+#include <vector>
+#include <exception>
+#include "lcc/value.hpp"
+
+namespace lcc{
+
+// A multidimensional stack. Stores stacks and associates them with a value.
+class MultiStack{
+public:
+    using ValStack = std::stack<Value>;
+    ValStack& create_stack(Value const& key) { return values.emplace_back(key, ValStack{}).second; }
+
+    ValStack& operator[](Value const& key) {
+        for(auto& [s_key, stack] : values)
+            if (s_key == key)
+                return stack;
+        throw std::runtime_error("Could not find key in multistack.");
+    }
+
+private:
+    std::vector<std::pair<Value, ValStack>> values;
+};
+
+}
+
+#endif // LCC_MULTI_STACK_HPP
