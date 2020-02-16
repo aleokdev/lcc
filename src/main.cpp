17dd14427dedba3e4784b20f445550efdfb498cc
alexdevteam <alexinfdev@gmail.com>
2020-02-16 14:42:44 +0100

    Do not run instance if not successful

--- src/main.cpp
+++ src/main.cpp
@@ -16,19 +16,20 @@ int main() {
         std::getline(std::cin, input);
         if(input == "exit")
             return 1;
+        std::cout.flush();
         lcc::Program prg;
+        bool success = false;
         try{
             prg = lcc::Program::from_string(input);
+            success = true;
         }catch(std::exception const& e){
             std::cerr << "[FATAL] Error while tokenizing: " << e.what() << std::endl;
         }
-        try{
+        if (success) try{
             lcc::Instance(prg).run("");
         }catch(std::exception const& e){
             std::cerr << "[FATAL] Runtime error: " << e.what() << std::endl;
         }
         std::cerr.flush();
-        std::cout << std::endl;
-        std::cout.flush();
     }
 }
